1300e04a6efbff32fac1b4301e710d53c4c64644
Robert Pasdziernik <ropfoo@gmail.com>
2020-02-25 14:53:52 +0100

    Restructured app.js and added countDown object to make things way more scaleable - using js Date System now

--- apps.js
+++ apps.js
@@ -1,58 +1,142 @@
 const countInput = document.getElementById('count-input');
 const startBtn = document.getElementById('start-btn');
+const inputWrapper = document.getElementById('input-wrapper');
+const durationSelector = document.getElementById('duration-selector');
 
 const countDownNumber = document.getElementById('count-down-number');
-let number = countDownNumber.dataset.number;
+let initNumber = 20;
 const numberWrapper = document.createElement('h1');
-numberWrapper.textContent = number;
 countDownNumber.appendChild(numberWrapper);
 
-let running = false;
+const countDown = {
+  running: false,
+  number: initNumber,
+  endTime: new Date(),
+  wasPaused: false,
+  pauseTime: new Date(),
+  durationState: 's',
+  timeValues: {
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0
+  },
+  countInterval: '',
+  start() {
+    //set inital endTime to current time
+    this.endTime = new Date();
+    //Check which state the user selected
+    //Add timer time to current time (endTime)
+    if (this.durationState === 's') {
+      if (!this.wasPaused) {
+        console.log('num is:' + this.number);
+        this.endTime.setSeconds(this.endTime.getSeconds() + this.number);
+      } else {
+        this.setTime('s', this.endTime);
+      }
+    } else if (this.durationState === 'm') {
+      if (!this.wasPaused) {
+        this.endTime.setMinutes(this.endTime.getMinutes() + this.number);
+      } else {
+        this.setTime('m', this.endTime);
+      }
+    }
+
+    this.countInterval = setInterval(() => {
+      if (this.running) {
+        const distance = this.endTime.getTime() - new Date().getTime();
+
+        this.timeValues.days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        this.timeValues.hours = Math.floor(
+          (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        );
+        this.timeValues.minutes = Math.floor(
+          (distance % (1000 * 60 * 60)) / (1000 * 60)
+        );
+        this.timeValues.seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        this.pauseTime = new Date();
+        this.displayTime();
+      }
+    }, 1000);
+  },
+  stop() {
+    this.running = false;
+    this.wasPaused = true;
+    clearInterval(this.countInterval);
+    // console.log('Pause Time:' + this.endTime);
+    this.displayTime();
+    // this.endTime.setTime();
+  },
+  displayTime() {
+    if (this.durationState === 's') {
+      numberWrapper.textContent = this.timeValues.seconds + 's ';
+    } else if (this.durationState === 'm') {
+      numberWrapper.textContent =
+        this.timeValues.minutes + 'm ' + this.timeValues.seconds + 's ';
+    } else {
+      numberWrapper.textContent =
+        this.timeValues.days +
+        'd ' +
+        this.timeValues.hours +
+        'h ' +
+        this.timeValues.minutes +
+        'm ' +
+        this.timeValues.seconds +
+        's ';
+    }
+  },
+  setTime(state, time) {
+    if (state === 's') {
+      time.setSeconds(time.getSeconds() + this.timeValues.seconds);
+      time.setMinutes(time.getMinutes());
+      time.setHours(time.getHours());
+    } else if (state === 'm') {
+      time.setSeconds(time.getSeconds() + this.timeValues.seconds);
+      time.setMinutes(time.getMinutes() + this.timeValues.minutes);
+      time.setHours(time.getHours());
+    }
+  },
+  setNumber(num) {
+    this.number = num;
+  }
+};
+numberWrapper.textContent = initNumber + countDown.durationState;
+
 let startActive = true;
-let currentNumber = number;
+
+durationSelector.addEventListener('change', e => {
+  countDown.durationState = e.target.value;
+  numberWrapper.textContent = countDown.number + countDown.durationState;
+  console.log(e.target.value);
+});
 
 countInput.addEventListener('input', e => {
-  number = Number(e.target.value);
-  currentNumber = number;
-  stopCountdown();
+  console.log(e.target.value);
+  countDown.setNumber(Number(e.target.value));
+  console.log('cd Num: ' + countDown.number);
+  countDown.wasPaused = false;
+
+  numberWrapper.textContent = countDown.number + countDown.durationState;
+
+  //countDown.number = number;
+  //countDown.stop();
 });
 
 startBtn.addEventListener('click', e => {
   if (startActive) {
-    running = true;
-    countDown(currentNumber);
+    countDown.running = true;
+    countDown.start(countDown.number);
     countInput.value = '';
     startActive = false;
     e.target.textContent = 'stop';
     e.target.style.background = 'indianred';
-    countInput.classList.add('input-invisible');
+    inputWrapper.classList.add('input-wrapper-invisible');
   } else {
-    stopCountdown();
+    countDown.stop();
     startActive = true;
-    e.target.style.background = 'greenyellow';
+    e.target.style.background = 'rgb(194, 255, 103)';
     e.target.textContent = 'start';
-    countInput.classList.remove('input-invisible');
+    inputWrapper.classList.remove('input-wrapper-invisible');
   }
 });
-
-const stopCountdown = () => {
-  running = false;
-  numberWrapper.textContent = currentNumber;
-};
-
-const countDown = num => {
-  let i = 0;
-  setTimeout(() => {
-    if (running) {
-      num -= 1;
-      numberWrapper.textContent = num;
-      currentNumber = num;
-      i++;
-      if (i <= num) {
-        countDown(num);
-      }
-    } else {
-      currentNumber = num;
-    }
-  }, 1000);
-};
